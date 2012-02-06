66b8006e151d9cad26d4778d5942aeed7ecbc81f
Thomas J Bradley <bradlet@algonquincollege.com>
2012-02-06 14:31:45 -0500

    Added list and delete pages for our dinosaur app.

---
+++ index.php
@@ -0,0 +1,57 @@
+<?php
+
+require_once '../db.php';
+
+// `->exec()` allows us to perform SQL and NOT expect results
+// `->query()` allows us to perform SQL and expect results
+$results = $db->query('
+	SELECT id, dino_name, period
+	FROM dinosaurs
+	ORDER BY dino_name ASC
+');
+
+?><!DOCTYPE HTML>
+<html>
+<head>
+	<meta charset="utf-8">
+	<title>Dinosaurs!</title>
+</head>
+<body>
+	
+	<ul>
+	<?php
+	/*
+		foreach ($results as $dino) {
+			echo '<li>' . $dino['dino_name'] . '</li>';
+		}
+	*/
+	?>
+	
+	<?php foreach ($results as $dino) : ?>
+		<li>
+			<a href="single.php?id=<?php echo $dino['id']; ?>"><?php echo $dino['dino_name']; ?></a>
+			&bull;
+			<a href="delete.php?id=<?php echo $dino['id']; ?>">Delete</a>
+		</li>
+	<?php endforeach; ?>
+	</ul>
+	
+</body>
+</html>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
